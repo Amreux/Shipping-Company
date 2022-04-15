70f8fb8136c39153cf81360b198ebe96738350ab
FaresAtef1 <96792115+FaresAtef1@users.noreply.github.com>
2022-04-16 01:10:13 +0200

    ..

---
+++ Cargo.h
@@ -0,0 +1,9 @@
+#pragma once
+class Truck
+{
+	int PreparationTime[2];
+	int LoadUnloadTime[2];
+	char CargoType;
+	int DeliveryDistance;
+	int cost;
+};
--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main()
 {
-	cout << "fares mtnak";
+	cout << "fares";
 	return 0;
 }
