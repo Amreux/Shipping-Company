6a6c5cd2e9a4f99de5f3df5c381209ec8802e2fe
Ghaithq <gaoia123@gmail.com>
2022-04-18 04:01:20 +0200

    Execute

--- PreparationEvent.cpp
+++ PreparationEvent.cpp
@@ -1,1 +1,9 @@
 #include "PreparationEvent.h"
+
+
+void PreparationEvent::Execute(Cargo* C) {
+	if (C->CargoType == 'N')
+	{
+
+	}
+}
--- PreparationEvent.h
+++ PreparationEvent.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "Event.h"
+#include "Cargo.h"
 
 class PreparationEvent:public Event
 {
-
+public:
+	void Execute(Cargo* C);
 };
 
