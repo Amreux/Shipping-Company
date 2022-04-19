f0d4ac974a71d95e13c45246857d08f8935195ea
FaresAtef1 <96792115+FaresAtef1@users.noreply.github.com>
2022-04-19 20:16:56 +0200

    d

--- LinkedList.h
+++ LinkedList.h
@@ -1,7 +1,37 @@
 #pragma once
+#include "Node.h"
+
 template <class T>
 class LinkedList
 {
+	Node* Head;
+
+public:
+	LinkedList()
+	{
+		Head = nullptr;
+	}
+
+	bool IsEmpty()
+	{
+		return (Head == NULL);
+	}
+
+	bool insert(T data)
+	{
+		Node<T>* NewNode = new Node<T>(data, nullptr);
+		if (IsEmpty())
+		{
+			Head = NewNode;
+		return true;
+		}
+		Node<T>* Temp = Head;
+		while (Temp->GetNext())
+			Temp = Temp->GetNext();
+		Temp->SetNext(NewNode);
+		return true;
+	}
+
 
 };
 
